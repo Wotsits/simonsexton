a324ebb80b3d9e1fd8d66037f0566fa79b854d30
Simon Sexton <simon.sexton@live.co.uk>
2023-12-20 21:22:47 +0000

    feat: convert hero to svg

--- js/script.js
+++ js/script.js
@@ -3,33 +3,91 @@ function openCloseNav() {
   nav.classList.toggle("open");
 }
 
+function selectLightMode() {
+  const root = document.querySelector(":root");
+  root.style.setProperty("--background-color", lightColors.background);
+  root.style.setProperty("--first-color", lightColors.first);
+  root.style.setProperty("--second-color", lightColors.second);
+  root.style.setProperty("--third-color", lightColors.third);
+  root.style.setProperty("--fourth-color", lightColors.fourth);
+  root.style.setProperty("--text-color", lightColors.text);
+  root.style.setProperty("--menu-background-color", lightColors.menu);
+  root.style.setProperty("--box-shadow", lightColors.boxShadow);
+    root.style.setProperty("--border", lightColors.border);
+}
+
+function selectDarkMode() {
+  const root = document.querySelector(":root");
+  root.style.setProperty("--background-color", darkColors.background);
+  root.style.setProperty("--first-color", darkColors.first);
+  root.style.setProperty("--second-color", darkColors.second);
+  root.style.setProperty("--third-color", darkColors.third);
+  root.style.setProperty("--fourth-color", darkColors.fourth);
+  root.style.setProperty("--text-color", darkColors.text);
+  root.style.setProperty("--menu-background-color", darkColors.menu);
+  root.style.removeProperty("--box-shadow");
+  root.style.setProperty("--border", darkColors.border);
+}
+
 const words = [
-    "refined",
-    "sublime",
-    "robust",
-    "functional",
-    "elegant",
-    "sophisticated",
-    "intuitive",
-    "powerful",
-    "beautiful",
-    "responsive",
-    "versatile",
-    "dynamic",
-    "flexible",
-    "efficient",
-    "secure",
-    "reliable",
-    "scalable",
-    "customizable",
-    "extensible",
-    "accessible",
-    "inclusive",
-  ];
+  "refined",
+  "sublime",
+  "robust",
+  "functional",
+  "elegant",
+  "sophisticated",
+  "intuitive",
+  "powerful",
+  "beautiful",
+  "responsive",
+  "versatile",
+  "dynamic",
+  "flexible",
+  "efficient",
+  "secure",
+  "reliable",
+  "scalable",
+  "customizable",
+  "extensible",
+  "accessible",
+  "inclusive",
+];
+
+const darkColors = {
+  background: "#111",
+  first: "#DE29BE",
+  second: "#333",
+  third: "#aaa",
+  fourth: "#6c63ff",
+  text: "#fff",
+  menu: "rgba(1, 1, 1, 0.9)",
+  boxShadow: undefined,
+  border: "solid 1px #white"
+};
+
+const lightColors = {
+  background: "#fff",
+  first: "#6c63ff",
+  second: "#eee",
+  third: "#333",
+  fourth: "#DE29BE",
+  text: "#111",
+  menu: "rgba(255, 255, 255, 0.9)",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+    border: "solid 1px #333"
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  const hero = document.querySelector(".hero-content");
+  const hero = document.querySelector("#hero-text");
   const highlight = hero.querySelector(".highlight");
+
+  const osDayOrNight = window.matchMedia("(prefers-color-scheme: light)");
+  
+  if (osDayOrNight.matches) {
+    selectLightMode();
+  } else {
+    selectDarkMode();
+  }
 
   setInterval(() => {
     highlight.classList.add("opacity-0");
