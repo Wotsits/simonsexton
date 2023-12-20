b5cbee39e35a96b20aea6f2f8ccc6c30eb61841d
Simon Sexton <simon.sexton@live.co.uk>
2023-12-20 17:52:05 +0000

    feat: remove animation and change colour scheme

--- js/script.js
+++ js/script.js
@@ -1,53 +1,42 @@
 function openCloseNav() {
-    const nav = document.getElementById("mobile-nav-menu");
-    nav.classList.toggle("open");
-    const body = document.querySelector("body");
+  const nav = document.getElementById("mobile-nav-menu");
+  nav.classList.toggle("open");
 }
 
-const blobColors = [
-    "#f2f2f2",
-    "#2f2e41",
-    "#fff",
-    "#e6e6e6",
-    "#6c63ff",
-]
+const words = [
+    "refined",
+    "sublime",
+    "robust",
+    "functional",
+    "elegant",
+    "sophisticated",
+    "intuitive",
+    "powerful",
+    "beautiful",
+    "responsive",
+    "versatile",
+    "dynamic",
+    "flexible",
+    "efficient",
+    "secure",
+    "reliable",
+    "scalable",
+    "customizable",
+    "extensible",
+    "accessible",
+    "inclusive",
+  ];
 
 document.addEventListener("DOMContentLoaded", () => {
-    const blobs = document.querySelectorAll(".blob");
-    const headAndHair = document.querySelector(".head-and-hair");
-    const headphones = document.querySelector(".headphones");
+  const hero = document.querySelector(".hero-content");
+  const highlight = hero.querySelector(".highlight");
 
-    setInterval(() => {
-        blobs.forEach(blob => {
-            const randomColor = Math.floor(Math.random() * blobColors.length);
-            blob.style.fill = blobColors[randomColor];
-        })
-    }, 500)
-
-    let counter = 0;
-
-    setInterval(() => {
-
-        const rectHeadAndHair = headAndHair.getBoundingClientRect();
-        const rectHeadphones = headphones.getBoundingClientRect();
-        
-        const centerXHeadAndHair = rectHeadAndHair.left + rectHeadAndHair.width;
-        const centerYHeadAndHair = rectHeadAndHair.top + rectHeadAndHair.height;
-
-        const centerXHeadphones = rectHeadphones.left + rectHeadphones.width;
-        const centerYHeadphones = rectHeadphones.top + rectHeadphones.height;
-
-
-        if (counter % 2 === 0) {
-            headAndHair.setAttribute("transform", "");
-            headphones.setAttribute("transform", "");
-        }
-        else {
-            headAndHair.setAttribute("transform", `rotate(-3 ${centerXHeadAndHair} ${centerYHeadAndHair})`);
-            headphones.setAttribute("transform", `rotate(-3 ${centerXHeadphones} ${centerYHeadphones})`);
-        }
-
-        counter++;
-    }, 2000)
-
-})
+  setInterval(() => {
+    highlight.classList.add("opacity-0");
+    setTimeout(() => {
+      const randomWord = Math.floor(Math.random() * words.length);
+      highlight.textContent = words[randomWord];
+      highlight.classList.remove("opacity-0");
+    }, 2000);
+  }, 8000);
+});
